af179790e6e58294bb91b2001b8bfdefccf9a066
Sarah Paz <sarah.paz@gmail.com>
2019-02-02 05:52:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer
   attr_accessor :status
-  attr_reader :sender, :receiver. :amount
+  attr_reader :sender, :receiver, :amount
 
   def initialize(sender, receiver, amount)
     @sender = sender
